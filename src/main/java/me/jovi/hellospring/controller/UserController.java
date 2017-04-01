5a96b2ca79203797624d87557c9167df18e084a8
Jovi <joyce221088@163.com>
2017-04-01 22:13:17 +0800

    JPA 原生SQL查询

---
+++ src/main/java/me/jovi/hellospring/controller/UserController.java
@@ -0,0 +1,36 @@
+package me.jovi.hellospring.controller;
+
+import me.jovi.hellospring.entity.ReqResult;
+import me.jovi.hellospring.entity.User;
+import me.jovi.hellospring.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by joyce on 2017/4/1.
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/doLogin")
+    @ResponseBody
+    public ReqResult doLogin(User user){
+        boolean result = userService.checkPassword(user);
+
+        if (result){
+            return ReqResult.success("登录成功！");
+        }else{
+            return ReqResult.failure("登录失败！");
+        }
+    }
+}
